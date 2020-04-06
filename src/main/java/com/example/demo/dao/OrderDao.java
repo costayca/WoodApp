aef21eb74542bb722ec226fa618251e414b227f7
Costel Corbuleac <corbuleacconstantin@gmail.com>
2020-04-06 20:27:02 +0300

    Added interface Dao for Orders

--- src/main/java/com/example/demo/dao/OrderDao.java
+++ src/main/java/com/example/demo/dao/OrderDao.java
@@ -1,4 +1,25 @@
 package com.example.demo.dao;
 
+import com.example.demo.model.Order;
+import com.example.demo.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface OrderDao {
+    int insertOrder(UUID id, Order order);
+
+    default int insertOrder(Order order) {
+        UUID id = UUID.randomUUID();
+        return insertOrder(id, order);
+    }
+
+    List<Order> selectAllOrders();
+
+    Optional<Order> selectOrderById(UUID id);
+
+    int deleteOrderById(UUID id);
+
+    int updateOrderById(UUID id, Person person);
 }
